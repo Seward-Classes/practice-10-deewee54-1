9ec7b0d609192bcc2dde8202b27975130871e4a4
deewee54 <decrosswhite@ualr.edu>
2026-04-14 19:39:01 -0500

    Update Shape.h

--- Shape.h
+++ Shape.h
@@ -1,35 +1,41 @@
-#ifndef SHAPE_H
-#define SHAPE_H
-
+#pragma once
 #include <iostream>
-#include <string>
 
+// Base abstract class
 class Shape {
 public:
     virtual double getArea() const = 0;
-    
-    virtual void display() const;
-    
+
+    virtual void display() const {
+        std::cout << "Generic Shape" << std::endl;
+    }
+
     virtual ~Shape() = default;
 };
 
+// Rectangle class
 class Rectangle : public Shape {
 private:
     double width;
     double height;
+
 public:
     Rectangle(double w, double h);
+
     double getArea() const override;
+
     void display() const override;
 };
 
+// Circle class
 class Circle : public Shape {
 private:
     double radius;
+
 public:
     Circle(double r);
+
     double getArea() const override;
+
     void display() const override;
 };
-
-#endif
