caf87460fe378a1158924a702328cbc89f55b512
deewee54 <decrosswhite@ualr.edu>
2026-04-14 19:39:57 -0500

    Update Shape.cpp

--- Shape.cpp
+++ Shape.cpp
@@ -1,26 +1,24 @@
 #include "Shape.h"
-#include <iostream>
 #include <cmath>
 
-
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-
-void Shape::display() const {
-    std::cout << "Generic Shape";
-}
-
-// Rectangle Implementation
+// Rectangle
 Rectangle::Rectangle(double w, double h) : width(w), height(h) {}
-double Rectangle::getArea() const { return width * height; }
-void Rectangle::display() const {
-    std::cout << "Rectangle (" << width << " x " << height << ")";
+
+double Rectangle::getArea() const {
+    return width * height;
 }
 
-// Circle Implementation
+void Rectangle::display() const {
+    std::cout << "Rectangle (" << width << " x " << height << ")" << std::endl;
+}
+
+// Circle
 Circle::Circle(double r) : radius(r) {}
-double Circle::getArea() const { return M_PI * radius * radius; }
+
+double Circle::getArea() const {
+    return M_PI * radius * radius;
+}
+
 void Circle::display() const {
-    std::cout << "Circle (radius: " << radius << ")";
+    std::cout << "Circle (radius: " << radius << ")" << std::endl;
 }
